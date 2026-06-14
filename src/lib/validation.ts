511595d634b2d4de7ed7b51a26604e75a59c6676
Cdr-Jie <CHANZHIJIE_BI23@iluv.ums.edu.my>
2026-06-14 10:43:41 +0800

    quick changes

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -4,4 +4,3 @@ export const passwordSchema = z
 .string()
 .min(1, { message: "Password is required"})
 .min(8, { message: "Password must be at least 8 characters long"})
-.regex(/[^A-Za-z0-9]/, { message: "Password must contain at least one special character"});
